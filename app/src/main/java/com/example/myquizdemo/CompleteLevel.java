4b490e21107ae7dfb576a3857b5ff236b757e49c
ezghou <www.egrenzgo@gmail.com>
2023-05-01 16:06:00 +0800

    Created a new activity `CompleteLevel`

---
+++ app/src/main/java/com/example/myquizdemo/CompleteLevel.java
@@ -0,0 +1,14 @@
+package com.example.myquizdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CompleteLevel extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complete_level);
+    }
+}
